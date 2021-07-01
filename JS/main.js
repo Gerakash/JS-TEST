5643e713708b6c4a820fd2146ecb6b02d0486630
gerakash <aigerim.kash@icloud.com>
2021-07-01 11:39:37 +0600

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,107 @@
+// logical operators 
+
+// console.log(true && false);
+// console.log(true && true); 
+
+//if
+// console.log(true || false)
+// console.log(false || false)
+// console.log(true || true)
+
+// console.log(!true);
+
+
+// true || false && true;
+// true || false;
+// true
+// console.log(true && (false || (true || false)));
+// console.log(true && (false || (true || false)));
+// console.log(true && (false || true));
+// console.log(true && true);
+// console.log(true)
+
+
+// conditional operators 
+
+// let response = prompt('how old are you?');
+// if (response >=18){
+//     alert('Allowed')
+// }else if(confirm('Do your patrents allow?')){
+//     alert('ok')
+// }else{
+//     alert('Forbidden')
+// }
+
+
+// let age = prompt('Enter your age');
+// let name = prompt('Enter your name').toLowerCase; 
+// if (age >= 18 && name === 'admin'){   
+//     alert('Welcome, Admin!')
+// }else if (age >= 18){
+//     alert('hello user!')
+// }else{
+//     alert('Forbidden')
+// }
+
+
+//switch - case
+
+// let sum = 5 + 3;
+// switch(sum){
+//     case 6: alert('not enough')
+//     break
+//     case 7: alert('incorrect')
+//     break
+//     case 8: alert('correct')
+//     break
+//     case 9: alert('too much')
+//     break
+//     default: alert('none')
+// }
+
+// let age = +prompt('Enter your age');        //hello
+// console.log(age)
+// switch(age){
+//     case 15: alert('Watch Shrek')
+//     break
+//     case 18: alert('Watch Naruto')
+//     break
+//     case 20: alert('Watch Avengers')
+//     break
+//     default: alert ('Watch 1+1')
+// }
+
+
+
+// let age = parseInt(prompt('Enter your age'));      
+// console.log(age)
+// switch(age){
+//     case 15: alert('Watch Shrek')
+//     break
+//     case 18: alert('Watch Naruto')
+//     break
+//     case 20: alert('Watch Avengers')
+//     break
+//     default: alert ('Watch 1+1')
+// }
+
+
+// ternary operator 
+
+// let age = prompt('Enter your age')
+// age >= 18 ? alert('Allowed') : alert('Forbidden') 
+
+// let a = 5;
+// a >= 5 ? a += 5 : a -= 2;
+// console.log(a);
+
+
+
+// if(a > 5){
+//     a += 5
+//     // a = a + 5
+// }else{
+//     a -= 2
+//     // a = a - 2
+// }
+// console.log(a);
